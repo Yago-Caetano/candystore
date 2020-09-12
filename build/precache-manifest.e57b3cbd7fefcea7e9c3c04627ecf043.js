5f5d8b55c247834774fa7bd130d35684692620d7
Yago-Caetano <46303870+Yago-Caetano@users.noreply.github.com>
2020-09-12 18:40:03 -0300

    BASE

---
+++ build/precache-manifest.e57b3cbd7fefcea7e9c3c04627ecf043.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429c366335c704ea66090543cd2778e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53c5943f5a918b74c092",
+    "url": "/static/css/2.bf0413c3.chunk.css"
+  },
+  {
+    "revision": "622ee2cfc79fa86a5f1b",
+    "url": "/static/css/main.a4bb718e.chunk.css"
+  },
+  {
+    "revision": "53c5943f5a918b74c092",
+    "url": "/static/js/2.3e367b11.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3e367b11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "622ee2cfc79fa86a5f1b",
+    "url": "/static/js/main.a423af0b.chunk.js"
+  },
+  {
+    "revision": "e3cac1ea12a0f657dd50",
+    "url": "/static/js/runtime-main.ffbe15ca.js"
+  },
+  {
+    "revision": "8ebdf167ab5160b66f1d844dc96e3db6",
+    "url": "/static/media/arvore.8ebdf167.svg"
+  },
+  {
+    "revision": "776291ba77d1e85e60dd4276e74ba5f3",
+    "url": "/static/media/barra_red.776291ba.jpeg"
+  },
+  {
+    "revision": "44a446abb83cd0eb9a1180523972319d",
+    "url": "/static/media/barra_white.44a446ab.jpeg"
+  },
+  {
+    "revision": "5776c79180ed3f424c7670d946501354",
+    "url": "/static/media/basket.5776c791.svg"
+  },
+  {
+    "revision": "b363bfda4964359b962e363ff012b015",
+    "url": "/static/media/bombons.b363bfda.jpg"
+  },
+  {
+    "revision": "80ff37a3d27413bbd747ff5c646b5994",
+    "url": "/static/media/candy_icon.80ff37a3.svg"
+  },
+  {
+    "revision": "d8b341700cd6b16986fccf2e4823a33e",
+    "url": "/static/media/coracao-recheado.d8b34170.jpg"
+  },
+  {
+    "revision": "3057ff5afd502f930b81385b92c21097",
+    "url": "/static/media/easter.3057ff5a.svg"
+  },
+  {
+    "revision": "249d50f7e96454883acd15e6703b8984",
+    "url": "/static/media/encomenda.249d50f7.svg"
+  },
+  {
+    "revision": "4fc89a3efbc4d35b97c6fc039b183820",
+    "url": "/static/media/entrega.4fc89a3e.svg"
+  },
+  {
+    "revision": "d08e7bf4752549563afaee4fcd208b9d",
+    "url": "/static/media/father.d08e7bf4.svg"
+  },
+  {
+    "revision": "ecd54862d008737c0f2df36566d98c30",
+    "url": "/static/media/lapidado.ecd54862.jpg"
+  },
+  {
+    "revision": "ab5b520cdb777d205d864c145fd3fbdb",
+    "url": "/static/media/logoFcb.ab5b520c.svg"
+  },
+  {
+    "revision": "0978f4fe1662d858a93c9101c6eb3f25",
+    "url": "/static/media/logoInsta.0978f4fe.svg"
+  },
+  {
+    "revision": "05312a2d8cf18610a38207112e62806c",
+    "url": "/static/media/logoWpp.05312a2d.svg"
+  },
+  {
+    "revision": "cbf89eb3bb88383ed4c4ac4acb689ee9",
+    "url": "/static/media/mainLogo.cbf89eb3.svg"
+  },
+  {
+    "revision": "f02ecb4d9d493e0f6c439a4305b1a7ae",
+    "url": "/static/media/mother.f02ecb4d.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "b8654dafce1c806cc4a160c240846814",
+    "url": "/static/media/torta.b8654daf.jpg"
+  },
+  {
+    "revision": "fdb286f08ecfebe852af62d0a1ade3bc",
+    "url": "/static/media/valentine.fdb286f0.svg"
+  },
+  {
+    "revision": "9517a704f03850bfdcd9cf9f338abded",
+    "url": "/static/media/woman.9517a704.svg"
+  }
+]);
